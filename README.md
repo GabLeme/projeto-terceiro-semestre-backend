388be0f084a25cbcc1831e355593ac1652d8207b
GabLeme <37668239+GabLeme@users.noreply.github.com>
2019-03-07 19:06:12 -0300

    Configuração Inicial do Projeto
    Configuração inicial do projeto para criarmos as APIs utilizando Spring Boot e MongoDB.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # projeto-terceiro-semestre
 Plataforma de serviço a idosos. [Projeto acadêmico]
+
+
+Branch criada para versionamento de código backend, criando serviços REST.
+Spring Boot + MongoDB + Web.
---
+++ eCare/src/main/java/io/eCare/eCare/ECareApplication.java
@@ -0,0 +1,13 @@
+package io.eCare.eCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECareApplication.class, args);
+	}
+
+}
