9b60fa99140e6cd8822b3fb191066c1f81091556
mateus009 <37670583+mateus009@users.noreply.github.com>
2019-05-01 20:44:51 -0300

    atualizando

--- eCare/src/main/java/io/eCare/eCare/Usuario.java
+++
@@ -1,9 +0,0 @@
-package io.eCare.eCare;
-
-import io.eCare.eCare.models.Consumers;
-
-public interface Usuario {
-    public Consumers consumerLogin(String Email, String Password);
-    public void cadastro(Consumers consume);
-
-}
